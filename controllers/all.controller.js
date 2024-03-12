3944fccd7318673cd0748316e49b7a02ae69958b
Sutirtha Rej <sutirtharej@Sutirthas-MacBook-Air-2.local>
2024-03-13 00:24:25 +0530

    chanegs

--- controllers/all.controller.js
+++ controllers/all.controller.js
@@ -5,10 +5,15 @@ let userAnswers = [];
 const submitQuestion = (req, res) => {
   try {
     const { question, options } = req?.body;
+    if (!question || !options) return res.status(400).send({ success: false });
     console.log("body", req?.body);
+
+    if (allQuestions.length > 5 && allQuestions.length > 1) allQuestions = [];
+    if (allOptions.length > 5 && allOptions.length > 1) allOptions = [];
+
     allQuestions.push(question);
     allOptions.push(options);
-    console.log("allQuestions--", allQuestions);
+    console.log("allQuestions--", allQuestions, allOptions);
     // example questions and answers
     // question = {
     //     string: '1+2 = 3',
@@ -24,10 +29,10 @@ const submitQuestion = (req, res) => {
     //   correct_opt: "opt_a",
     // };
     res.status(200).send({
-        message: '',
-        data: {allQuestions: allQuestions, allOptions: allOptions},
-        success: true
-    })
+      message: "",
+      data: { allQuestions: allQuestions, allOptions: allOptions },
+      success: true,
+    });
   } catch (err) {
     return res.status(500).send({
       message: "Internal server error",
@@ -38,31 +43,70 @@ const submitQuestion = (req, res) => {
 };
 
 const submitAnswer = (req, res) => {
-    try {
-        const { question_id, option_id, answer } = req?.body;
-        let result = true
-        // find the option and its answer
-        allOptions.map(each_option => {
-            const { question_id: qid, id: oid, correct_opt, opt_a, opt_b, opt_c } = each_option
-            if (question_id == qid && oid == option_id) {
-                result = answer == correct_opt ? true : false
-            }
-        })
-        res.status(200).send({
-            data: [{result}],
-            message: result ? 'Correct' : 'In-Correct',
-            success: true
-        })
-    } catch (err) {
-        return res.status(500).send({
-          message: "Internal server error",
-          success: false,
-          data: [],
-        });
-    }
-}
+  try {
+    const { question_id, option_id, answer } = req?.body;
+    let result = true;
+    // find the option and its answer
+    allOptions.map((each_option) => {
+      const {
+        question_id: qid,
+        id: oid,
+        correct_opt,
+        opt_a,
+        opt_b,
+        opt_c,
+      } = each_option;
+      if (question_id == qid && oid == option_id) {
+        result = answer == correct_opt ? true : false;
+      }
+    });
+    res.status(200).send({
+      data: [{ result }],
+      message: result ? "Correct" : "In-Correct",
+      success: true,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      message: "Internal server error",
+      success: false,
+      data: [],
+    });
+  }
+};
+
+const showLatestQuestion = (req, res) => {
+  try {
+    // const { question_id, option_id, answer } = req?.body;
+    // question = {
+    //     string: '1+2 = 3',
+    //     id: 1,
+    //     option_id: 1
+    // }
+    console.log('---allQuestions[allQuestions.length - 1]', allQuestions)
+    let latestQuestion = allQuestions[allQuestions.length - 1]?.string;
+    console.log("latest question", latestQuestion);
+    let latestOption = allOptions[allOptions.length - 1];
+    let opt_a = latestOption?.opt_a;
+    let opt_b = latestOption?.opt_b;
+    let opt_c = latestOption?.opt_c;
+    let correct_opt = latestOption?.correct_opt;
+
+    return res.status(200).send({
+      data: { latestQuestion, opt_a, opt_b, opt_c, correct_opt },
+      message: "",
+      success: true,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      message: "Internal server error",
+      success: false,
+      data: [],
+    });
+  }
+};
 
 module.exports = {
   submitQuestion,
-  submitAnswer
+  submitAnswer,
+  showLatestQuestion
 };
--- routes/all.route.js
+++ routes/all.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   submitQuestion,
   submitAnswer,
+  showLatestQuestion
 } = require("../controllers/all.controller");
 const router = express.Router();
 
 // Registration route
 router.post("/submit-question", submitQuestion);
 router.post("/submit-answer", submitAnswer);
+router.post("/show-latest-question", showLatestQuestion);
 
 module.exports = router;
